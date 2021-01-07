d9b8e6059abbf495053c8eeaf154184d972c487f
Andrew David <56502440+andydavidlee@users.noreply.github.com>
2021-01-07 13:15:10 +1100

    a

---
+++ vue-interface/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main-app" class="container">
+      <h1>{{title}}</h1>
+      <font-awesome-icon icon="plus" class="mr-2" />New Appointment
+      </div>
+</template>
+
+<script>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: 'MainApp',
+  data: function() {
+    return {
+      title: "Appointment List",
+    };
+  },
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
